82d39cd1bae2888b58ac8bf90eab08315c83efb4
Tung Teng <thetungctn@gmail.com>
2026-01-13 00:28:19 +0700

    Astro add tailwind

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -8,7 +8,7 @@ export default function Greeting({ messages }: { messages: string[] }) {
 
   return (
     <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
+      <h3 className="text-indigo-500">{greeting}! Thank you for visiting!</h3>
       <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
     </div>
   );
