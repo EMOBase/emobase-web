36c0e4a9c438f8fc836d0d48c07ccfad1606b9b8
Tung Teng <thetungctn@gmail.com>
2026-01-13 01:39:43 +0700

    Init shadcn & add button component

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import { Button } from "@/components/ui/button";
+
 export default function Greeting({ messages }: { messages: string[] }) {
   const randomMessage = () =>
     messages[Math.floor(Math.random() * messages.length)];
@@ -7,14 +9,11 @@ export default function Greeting({ messages }: { messages: string[] }) {
   const [greeting, setGreeting] = useState(messages[0]);
 
   return (
-    <div>
-      <h3 className="text-indigo-500">{greeting}! Thank you for visiting!</h3>
-      <button
-        onClick={() => setGreeting(randomMessage())}
-        className="cursor-pointer"
-      >
-        New Greeting
-      </button>
+    <div className="p-8">
+      <h3 className="text-indigo-500 mb-4">
+        {greeting}! Thank you for visiting!
+      </h3>
+      <Button onClick={() => setGreeting(randomMessage())}>New Greeting</Button>
     </div>
   );
 }
