0892bfbd0a91d12c5ec0685f7fb4244ad6faf215
Tung Teng <thetungctn@gmail.com>
2026-01-28 02:58:07 +0700

    Show spinner as right addon to PubMed ID input when fetching

--- src/components/pages/details/PublicationCRUD/AddPublicationButton/PubMedIdField.tsx
+++ src/components/pages/details/PublicationCRUD/AddPublicationButton/PubMedIdField.tsx
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useStore } from "@tanstack/react-form";
 import { useDebounceValue } from "usehooks-ts";
 
 import { withForm } from "@/hooks/form/useAppForm";
+import { Spinner } from "@/components/ui/spinner";
 
 import { fetchPubMedArticle, type PubMedArticle } from "./pubmed";
 import formOptions from "./formOptions";
@@ -14,18 +15,22 @@ function isValidPMID(input: string) {
 const PubMedIdField = withForm({
   ...formOptions,
   render: ({ form }) => {
+    const [loading, setLoading] = useState(false);
+
     const pmid = useStore(form.store, (state) => state.values.pmid);
 
     const [debouncedPMID] = useDebounceValue(pmid, 500);
 
     useEffect(() => {
       if (!isValidPMID(pmid)) return;
+      setLoading(true);
       fetchPubMedArticle(debouncedPMID)?.then((result) => {
         if (result) {
           const fields = Object.keys(result) as (keyof PubMedArticle)[];
           fields.forEach((field) => {
             form.setFieldValue(field, result[field]);
           });
+          setLoading(false);
         }
       });
     }, [debouncedPMID]);
@@ -39,6 +44,7 @@ const PubMedIdField = withForm({
             type="number"
             placeholder="e.g. 18586236"
             leftAddon="PMID:"
+            rightAddon={loading && <Spinner />}
             className="[appearance:textfield] [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none"
           />
         )}
